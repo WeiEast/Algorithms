d9b00f5b8dfbe11782968b5f1e2feacefbc0dd90
BraydenLeo <ypyupeng@vip.qq.com>
2015-08-26 16:11:10 +0800

    二叉查找树
    Signed-off-by: BraydenLeo <ypyupeng@vip.qq.com>

---
+++ src/main/java/mywork/Factorial.java
@@ -0,0 +1,14 @@
+package mywork;
+
+public class Factorial {
+	public static void main(String[] args) {
+		System.out.println(getFactorial(100));
+	}
+
+	private static double getFactorial(int n) {
+		if (n == 1) {
+			return 1;
+		}
+		return n * getFactorial(n - 1);
+	}
+}
