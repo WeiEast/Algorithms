e9724723d79089c9eb91fc632c8f2bb636fd0a78
BraydenLeo <ypyupeng@vip.qq.com>
2015-10-10 15:18:32 +0800

    二分查找

---
+++ src/main/java/lintcode/America/binarySearch/BinarySearchSolution.java
@@ -0,0 +1,141 @@
+package lintcode.America.binarySearch;
+
+import lombok.extern.log4j.Log4j;
+
+/**
+ * 二分查找
+ *
+ */
+@Log4j
+public class BinarySearchSolution {
+	public int searchInsert(int[] A, int target) {
+		int i = 0;
+		int j = A.length;
+		int mid = (j - i) / 2 + i;
+
+		while (i < j && i < mid) {
+			if (A[mid] > target) {
+				j = mid;
+			} else if (A[mid] == target) {
+				break;
+			} else {
+				i = mid;
+			}
+			mid = (j - i) / 2 + i;
+		}
+		return i >= mid && mid != 0 ? i + 1 : mid;
+	}
+
+	public boolean searchMatrix(int[][] matrix, int target) {
+		if (matrix == null || matrix.length == 0)
+			return false;
+		int i = 0;
+		while (i < matrix.length && matrix[i][matrix[i].length - 1] < target)
+			i++;
+		if (i >= matrix.length)
+			return false;
+		int left = 0;
+		int right = matrix[i].length - 1;
+		while (left <= right) {
+			int mid = (left + right) / 2;
+			if (matrix[i][mid] > target)
+				right = mid - 1;
+			else if (matrix[i][mid] == target)
+				return true;
+			else
+				left = mid + 1;
+		}
+		return false;
+
+	}
+
+	public int woodCut(int[] L, int k) {
+		int max = 0;
+		for (int i = 0; i < L.length; i++) {
+			max = Math.max(max, L[i]);
+		}
+
+		// find the largest length that can cut more than k pieces of wood.
+		int start = 1, end = max;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (count(L, mid) >= k) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+
+		if (count(L, end) >= k) {
+			return end;
+		}
+		if (count(L, start) >= k) {
+			return start;
+		}
+		return 0;
+	}
+
+	private int count(int[] L, int length) {
+		int sum = 0;
+		for (int i = 0; i < L.length; i++) {
+			sum += L[i] / length;
+		}
+		return sum;
+	}
+
+	public int findFirstBadVersion(int n) {
+
+		int i = 1;
+		int j = n;
+		while (i <= j) {
+			int mid = (i + j) / 2;
+			if (VersionControl.isBadVersion(mid)) {
+				j = mid - 1;
+			} else {
+				i = mid + 1;
+			}
+
+		}
+		return i;
+
+	}
+
+	static class VersionControl {
+		public static boolean isBadVersion(int k) {
+			if (k >= 4)
+				return true;
+			return false;
+		};
+	}
+
+	/**
+	 * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+	
+		(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+		
+		You are given a target value to search. If found in the array return its index, otherwise return -1.
+		
+		You may assume no duplicate exists in the array.
+		
+		Have you met this question in a real interview? Yes
+		Example
+		For [4, 5, 1, 2, 3] and target=1, return 2.
+		
+		For [4, 5, 1, 2, 3] and target=0, return -1.
+		
+		Challenge
+		O(logN) time
+	 * @param A
+	 * @param target
+	 * @return
+	 */
+	public int search(int[] A, int target) {
+		
+	}
+
+	public static void main(String[] args) {
+		BinarySearchSolution b = new BinarySearchSolution();
+		log.info(b.findFirstBadVersion(5));
+	}
+
+}
--- src/main/java/lintcode/America/binarySearch/WoodCut.java
+++ src/main/java/lintcode/America/binarySearch/WoodCut.java
@@ -1,5 +1,9 @@
 package lintcode.America.binarySearch;
 
+import java.util.Arrays;
+
+import lombok.extern.log4j.Log4j;
+
 /**
  * 有一些原木，现在想把这些木头切割成一些长度相同的小段木头，需要得到的小段的数目至少为 k。当然，我们希望得到的小段越长越好，你需要计算能够得到的小段木头的最大长度。
 
@@ -14,35 +18,46 @@ package lintcode.America.binarySearch;
 O(n log Len), Len为 n 段原木中最大的长度
  *
  */
+@Log4j
 public class WoodCut {
 
 	public int woodCut(int[] L, int k) {
+		int max = 0;
+		for (int i = 0; i < L.length; i++) {
+			max = Math.max(max, L[i]);
+		}
+
+		// find the largest length that can cut more than k pieces of wood.
+		int start = 1, end = max;
+		while (start + 1 < end) {
+			int mid = start + (end - start) / 2;
+			if (count(L, mid) >= k) {
+				start = mid;
+			} else {
+				end = mid;
+			}
+		}
+
+		if (count(L, end) >= k) {
+			return end;
+		}
+		if (count(L, start) >= k) {
+			return start;
+		}
+		return 0;
+	}
+
+	private int count(int[] L, int length) {
 		int sum = 0;
 		for (int i = 0; i < L.length; i++) {
-			sum += L[i];
+			sum += L[i] / length;
 		}
-
-		int target = sum / k;
-
-		while (true) {
-			int g = 0;
-			for (int i = 0; i < L.length; i++) {
-				g += L[i] / target;
-			}
-			if (g >= k)
-				return target;
-			else {
-				target--;
-				if (target < k)
-					return 0;
-			}
-
-		}
-
+		return sum;
 	}
 
 	public static void main(String[] args) {
 		WoodCut w = new WoodCut();
+		log.info(w.woodCut(new int[] { 232, 124, 456 }, 7));
 
 	}
 }
